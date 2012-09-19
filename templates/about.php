836f4336a974f3341d41051811a8f3440ecc6ef4
Karl Oscar Weber <karloscarweber@gmail.com>
2012-09-19 13:40:45 -0600

    fixed typ in about.php

--- templates/about.php
+++ templates/about.php
@@ -7,7 +7,7 @@
 
     <p>My name is karl oscar weber</p>
 
-    <p>I'm a web developer and pretend iOS Engineer living and working in the Salt Lake City area. I work for Red Olive, A small creative agency excellent in Web, Print, Identity, and SEO. I experiment with web stuff, and try to make video games. During the week You can find me riding me cheap fixie to and from work, watching movies, coding, or hanging out with my family. You can follow me on twitter: <a href="http://www.twitter.com/karloscarweber" target="_blank">@karloscarweber</a>, or github: <a href="http://www.github.com/karloscarweber">@karloscarweber</a>, where I tweet slight nonsense and post ever improving code. </p>
+    <p>I'm a web developer and pretend iOS Engineer living and working in the Salt Lake City area. I work for Red Olive, A small creative agency excellent in Web, Print, Identity, and SEO. I experiment with web stuff, and try to make video games. During the week You can find me riding my fixie to and from work, watching movies, coding, or hanging out with my family. You can follow me on twitter: <a href="http://www.twitter.com/karloscarweber" target="_blank">@karloscarweber</a> where I tweet slight nonsense, or github: <a href="http://www.github.com/karloscarweber">@karloscarweber</a> where I post ever improving code. </p>
 
   </section>
 
