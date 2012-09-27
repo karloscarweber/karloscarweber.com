ff89a3586cf2164ae7eca3469c2ff61f61500310
Karl Weber <karloscarweber@gmail.com>
2012-09-26 20:07:26 -0600

    Made the site more mobile friendly.

--- templates/layout.php
+++ templates/layout.php
@@ -25,6 +25,11 @@
     <link rel="apple-touch-icon" href="/apple-touch-icon.png">
     <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">
     <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png">
+
+    <!-- Mobile viewport optimized: h5bp.com/viewport -->
+    <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
+
+
   </head>
 
   <body>
