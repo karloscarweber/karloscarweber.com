e89387bedbbdb1b048581164618021b18ab221de
Karl Weber <karloscarweber@gmail.com>
2013-01-09 23:12:13 -0700

    added some links to other.

--- templates/other.php
+++ templates/other.php
@@ -1,6 +1,5 @@
 <?php include("header.php"); ?>
 
-
     <div class="container">
 
       <div class="grater">
@@ -9,26 +8,31 @@
       <div class="content">
         <article class="post">
           <header>
-            <h1>Other Things:</h1>
+            <h3>Other Things:</h3>
           </header>
 
           <section class="content">
-
             <p><em>Please note, this page is not ready yet.</em></p>
-
             <p><a href="/git-guide">My guide to Git and Github</a>(a personal reference)</p>
             <p><a href="/cocos2d-guide">Link Bait and Guide to Cocos2d</a></p>
 
+            <p><em> iOS Links </em></p>
+            <ul>
+              <li><a href="http://http://nscookbook.com">NSCookBook</a></li>
+            </ul>
+
+            <br>
+
+            <p><em> Ruby Links </em></p>
+            <ul>
+              <li><a href="http://railscasts.com">Rails Casts</a></li>
+            </ul>
+
           </section>
-
         </article>
-
-          <br>
-          <hr>
-          <br>      </div><!-- End .content -->
+      </div><!-- End .content -->
 
 <?php include("footer.php"); ?>
-
 
     </div> <!-- End .container -->
   </body>
