aeeb688c36a8f2d4110026fb5dbaf2af32a6ed46
Karl Weber <karloscarweber@gmail.com>
2013-01-11 23:32:01 -0700

    Just one more change.

--- templates/blog.php
+++ templates/blog.php
@@ -4,23 +4,21 @@
 ?>
     <div class="grater">
     <article class="post">
-      <div>
-        <header>
-          <p class="date"><?php  echo date($global['date.format'],strtotime($article['meta']['date']));  ?></p>
-          <h2><a href="<?php echo $article['url']; ?>"><?php echo $article['meta']['title']; ?></a></h2>
-        </header>
+      <header>
+        <p class="date"><?php  echo date($global['date.format'],strtotime($article['meta']['date']));  ?></p>
+        <h2><a href="<?php echo $article['url']; ?>"><?php echo $article['meta']['title']; ?></a></h2>
+      </header>
 
-        <section class="content">
-          <p>
-            <?php echo substr(strip_tags($article['content']), 0,530); ?>...
-          </p>
-        </section>
+      <section class="content">
+        <p>
+          <?php echo substr(strip_tags($article['content']), 0,530); ?>...
+        </p>
+      </section>
 
-        <div class="more"><a href="<?php echo $article['url']; ?>">more</a></div>
-        <footer>
+      <div class="more"><a href="<?php echo $article['url']; ?>">more</a></div>
+      <footer>
 
-        </footer>
-      </div>
+      </footer>
     </article>
     </div><!-- End .grater -->
 
