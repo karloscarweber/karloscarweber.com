25739a272f0fe0979f312683252b4a781de2cabb
Karl Weber <karloscarweber@gmail.com>
2013-01-28 11:45:17 -0700

    just checking to see if this works?

--- templates/git-guide.php
+++ templates/git-guide.php
@@ -21,7 +21,7 @@
             This adds a file or set of files to the staging area.</p>
             <p><strong>git add -u</strong> <br>  
             Adds everything new to be the staging area. This is the command you'll find yourself using the most out of convenience.</p>
-            <p><strong>git commit -m "Commit message"</strong> <br>  
+            <p><strong>git commit -m "[Commit message]"</strong> <br>  
             This commits the staged changes to git, with a message. If you don't add the -m option then it makes you use vim, which takes forever!</p>
 
           </section>
