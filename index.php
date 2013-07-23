34a0f73b91cdc4d3d57b67a86b8700afab32b3b7
Karl Weber <karloscarweber@gmail.com>
2013-07-23 14:28:16 -0600

    just testing out some iOS response body stuff.

--- index.php
+++ index.php
@@ -1,31 +1,7 @@
 <?php
-/**
-* Require necessary files
-*/
-require 'Slim/Slim.php';
-require 'lib/Textpress.php';
-require 'lib/View.php';
 
-/**
-* Require config file
-* @return Array config values
-*/
-$config = require 'config/config.php';
 
-/**
-* Create an instance of Slim with custom view
-* and set the configurations from config file
-*/
 
-$app = new Slim(array('view' => 'View','mode' => 'production'));
-$app->config($config);
+echo "response";
 
-/**
-* Create an object of Textpress and pass the object of Slim to it.
-*/
-$textpress = new Textpress($app);
-
-/**
-* Finally run Textpress
-*/
-$textpress->run();
+die;
